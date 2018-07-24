03a3a238e7281181605ba481d18fa5374288b903
adikalova <adikalova@gmail.com>
2018-07-24 20:45:29 +0300

    added selectShowsFromList Test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -11,7 +12,6 @@ public class LoginTest {
 
 
     public static WebDriver driver;
-
 
 
     @BeforeTest
@@ -25,14 +25,30 @@ public class LoginTest {
         driver.get("https://en.myshows.me/");
     }
 
-    @Test(priority = 1 )
-   public void loginToMyshows(){
+    @Test
+    public void loginToMyshows() {
         driver.findElement(By.xpath(("//*[@class='modal_login']"))).click();
         driver.findElement(By.id("ajax_login_login")).sendKeys("Sunwel");
         driver.findElement(By.id("ajax_login_password")).sendKeys("A771d6552");
         driver.findElement(By.id("ajax_login")).click();
 
         Assert.assertTrue(driver.getTitle().contains("MyShows"), "Login is successful!");
+
+    }
+    @Test (priority = 1)
+    public void selectShowsFromList() throws InterruptedException {
+        String MyShow = "Lost";
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[contains(@href,'/search/all/')]")).click();
+        driver.findElement(By.xpath("//a[contains(@href,'myshows.me/view/8/')]")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/a")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[contains(@href,'/profile/favorites/')]")).click();
+
+        Assert.assertEquals((MyShow),"Lost");
+
+        //remove from favorites
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[2]/div/div/a[2]")).click();
 
     }
 }
